1daac4cacedc0d857d4ae785726c3d65acb8470a
Sirajul Muneer C B <sirajul.muneer@diagnal.com>
2018-06-05 13:39:46 +0530

    Fixing blox styles and left width

--- dist/grapes.js
+++ dist/grapes.js
@@ -23812,7 +23812,7 @@ module.exports = function () {
     plugins: plugins,
 
     // Will be replaced on build
-    version: '0.14.51',
+    version: '0.14.52',
 
     /**
      * Initializes an editor based on passed options
